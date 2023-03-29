004544928b778b723ac59dc26a41259e53697c09
Sergey <sergeymaksheev@gmail.com>
2023-03-29 21:58:48 +0300

    finished tests

--- main.py
+++ main.py
@@ -4,20 +4,22 @@ import tempfile
 import asyncio
 import hashlib
 from git.repo.base import Repo
+from decouple import config
 
 
 async def get_clone():
     """Get content from git repository in temporary dir."""
     with tempfile.TemporaryDirectory() as dir_temp:
-        Repo.clone_from("https://gitea.radium.group/radium/project-configuration.git", dir_temp)
+        Repo.clone_from(config('GIT_REPO_URL'), dir_temp)
         get_hash(dir_temp)
 
 
-def get_hash(dir_temp):
+def get_hash(dir_temp: str):
     """Gets files hash."""
     if isfile(dir_temp):
-        with open(dir_temp, 'rb') as f:
-            print(hashlib.sha256(f.read()).hexdigest())
+        with open(dir_temp, 'rb') as file:
+            file_hash = hashlib.sha256(file.read()).hexdigest()
+            print(file_hash) 
     else:
         for dirobject in listdir(dir_temp):
             if not dirobject.startswith("."):
@@ -31,7 +33,7 @@ def start():
         asyncio.ensure_future(get_clone()),
         asyncio.ensure_future(get_clone()),
     ]
-    
+
     event_loop = asyncio.get_event_loop()
     event_loop.run_until_complete(asyncio.gather(*clones))
     event_loop.close()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,21 +1,67 @@
 """Tests_for_main_module"""
-import responses
+from unittest import mock
 import pytest
-from datetime import datetime
+from pytest_mock import MockerFixture
+from unittest.mock import MagicMock, AsyncMock
+from main import get_clone, get_hash
 
 
-# @responses.activate
-# def test_start():
-#     valid_json_answer = {
-#         "lastActionTime": 1626615588,
-#         "timeDiff": 16984
-#     }
-#     responses.add(responses.GET, "https://www.avito.ru/web/user/get-status/177868588",
-#                   json=valid_json_answer, status=200)
-#     some_resource_client = SomeResourceClient("https://www.avito.ru")
-#     res = some_resource_client.get_user_last_action_time(177868588)
-#     assert res == datetime.fromtimestamp(valid_json_answer.get("lastActionTime") - valid_json_answer.get("timeDiff"))
+@pytest.fixture(name="isfile_mock")
+def isfile_fixture(mocker: MockerFixture) -> MagicMock:
+    """Isfile fixture"""
+    isfile_mock = MagicMock(return_value=False)
+    mocker.patch('main.isfile', isfile_mock)
+    return isfile_mock
 
-# def test_get_clone():
-#     valid_answer = {}
-#     responses.add(responses. , "https://gitea.radium.group/radium/project-configuration.git")
+
+@pytest.mark.asyncio
+async def test_get_clone_success(mocker: MockerFixture):
+    """Success."""
+    repo_clone_from_mock = MagicMock()
+    mocker.patch('main.Repo.clone_from', repo_clone_from_mock)
+    get_hash_mock = MagicMock()
+    mocker.patch('main.get_hash', get_hash_mock)
+    
+    await get_clone()
+    
+    repo_clone_from_mock.assert_called_once()
+    get_hash_mock.assert_called_once()
+
+
+def test_get_hash_success_recursion(mocker: MockerFixture,
+                                    isfile_mock: MagicMock):
+    """Success recursion"""
+    name = 'test_dir'
+    listdir_mock = MagicMock(return_value=[name])
+    mocker.patch('main.listdir', listdir_mock)
+
+    with mock.patch('main.get_hash') as mock_get_hash:
+        test_path = 'test_dir_name'
+        get_hash(test_path)
+        isfile_mock.assert_called_once()
+        listdir_mock.assert_called_once_with('test_dir_name')
+        assert mock_get_hash.called
+        assert mock_get_hash.call_count == 1
+        mock_get_hash.assert_called_once_with(dir_temp=f"{test_path}/{name}")
+
+
+def test_get_hash_success_print_hash(mocker: MockerFixture,
+                                     isfile_mock: MagicMock):
+    """Success print hash"""
+    open_mock = MagicMock()
+    open_mock.__enter__ = MagicMock(return_value='test')
+    
+    mocker.patch('main.open', open_mock)
+    isfile_mock.return_value = True
+    print_mock = MagicMock()
+    mocker.patch('main.print', print_mock)
+
+    hashlib_mock = MagicMock()
+    mocker.patch('main.hashlib', hashlib_mock)
+
+    test_file = 'test_file'
+    get_hash(test_file)
+    isfile_mock.assert_called_once()
+    open_mock.assert_called_once()
+    print_mock.assert_called_once()
+
