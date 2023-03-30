8a7ce6203923403e6f6ae260f0206c2188d8dd8b
Sergey <sergeymaksheev@gmail.com>
2023-03-30 19:23:59 +0300

    upgrade app

--- app.py
+++ app.py
@@ -6,30 +6,31 @@ import tempfile
 from os import listdir
 from os.path import isfile
 
+from awaits.awaitable import awaitable
 from decouple import config
 from git.repo.base import Repo
 
 list_hash = []
 
 
-async def run_clone(path_to_repo: str) -> None:
+@awaitable
+def run_clone(path_to_repo: str, dir_temp: str) -> None:
     """Get content from path_to_repo in temporary dir.
-    
+
     Args:
         path_to_repo: str
+        dir_temp: str
 
     """
-    with tempfile.TemporaryDirectory() as dir_temp:
-        Repo.clone_from(path_to_repo, dir_temp)
-        generate_hash(dir_temp)
+    Repo.clone_from(path_to_repo, dir_temp)
 
 
 def generate_hash(dir_temp: str) -> None:
     """Write list with file's name add hash in json from dir_temp.
-    
+
     Args:
         dir_temp: str
- 
+
     """
     if isfile(dir_temp):
         with open(dir_temp, "rb") as f_o:
@@ -51,11 +52,13 @@ async def start():
     """Start module, controls another functions."""
     repo_url = config("GIT_REPO_URL")
 
-    await asyncio.gather(
-        run_clone(repo_url),
-        run_clone(repo_url),
-        run_clone(repo_url),
-    )
+    with tempfile.TemporaryDirectory() as dir_temp:
+        await asyncio.gather(
+            run_clone(repo_url, dir_temp + "/1"),
+            run_clone(repo_url, dir_temp + "/2"),
+            run_clone(repo_url, dir_temp + "/3"),
+        )
+        generate_hash(dir_temp=dir_temp)
 
     with open("output.json", "w") as f_o:
         json.dump(list_hash, f_o)
